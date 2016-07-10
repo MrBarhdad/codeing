23e1051f3606b68b590c0d5eac4e706c59784ea3
Mohamed Barhdad <simo.baghdad@gmail.com>
2016-07-10 14:29:44 +0200

    Add files via upload

---
+++ format_helper.php
@@ -0,0 +1,18 @@
+<?php 
+
+function formatdate($date){
+
+return date('F j, Y, g:i a',strtotime($date));
+
+}
+
+
+function shortenText($text, $chars = 5450){
+
+	$text = $text." ";
+	$text = substr($text, 0, $chars);
+	$text = substr($text, 0 , strrpos($text, ' '));
+	$text = $text."";
+	return $text;
+}
+?>
